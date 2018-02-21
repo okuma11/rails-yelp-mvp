f422eb61c2f03bb977197272df4f4f09cbb54416
Nozomi Okuma <skyidentity@gmail.com>
2018-02-21 16:10:02 +0900

    restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '1234567',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '90123456',
+  }
+]
+Restaurant.create!(restaurants_attributes)
